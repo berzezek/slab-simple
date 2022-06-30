2485b3d368b39f51f035610b36169d35e62dc122
Berzezek <wknduz@gmail.com>
2022-06-30 17:56:39 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template lang="">
+  <div>
+    <main-page />
+  </div>
+</template>
+<script>
+import MainPage from './components/page/MainPage.vue'
+export default {
+  components: {
+    MainPage
+  }
+  
+}
+</script>
+<style lang="">
+  
+</style>
